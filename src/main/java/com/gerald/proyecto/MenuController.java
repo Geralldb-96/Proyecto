101adf143eff3e93593cb3543d7f61ec16e4eff8
Geralldb-96 <geraldine-rojast@unilibre.edu.co>
2025-08-17 13:44:36 -0500

    Actualización de la vista menu.html y controlador MenuController

--- src/main/java/com/gerald/proyecto/MenuController.java
+++ src/main/java/com/gerald/proyecto/MenuController.java
@@ -1,5 +1,14 @@
 package com.gerald.proyecto;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MenuController {
 
+    // Mapea la ruta raíz "/" y devuelve la vista "menu"
+    @GetMapping("/")
+    public String mostrarMenu() {
+        return "menu";  // Spring Boot busca 'menu.html' en src/main/resources/templates
+    }
 }
